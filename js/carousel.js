a5686cea1303843eed45c1e9d6462bff2df7cb79
Nico <nicosetzes@gmail.com>
2023-06-26 17:05:39 -0300

    V.1.7 I've modified the logic on the carousel: from now on, is slides automatically until 768px, then it works as a gallery and it displays 4 images at the same time. I've also worked on images across the website, I still have to compress them.

--- js/carousel.js
+++ js/carousel.js
@@ -1,19 +1,9 @@
 const multipleCardCarousel = document.querySelector("#carousel");
 
-// ¿Estoy en pantalla chica o mediana? //
+// mediumWindow será true si el ancho de pantalla es como mínimo 768
 
-const mobileWindow = window.matchMedia("(min-width: 400px)").matches;
-
-// mobileWindow será true si el ancho de pantalla es como mínimo 400, sino false
-
-const smallWindow =
-  window.matchMedia("(min-width: 400px)").matches &&
-  !window.matchMedia("(min-width: 768px)").matches;
-
-// smallWindow será true si el ancho de pantalla está entre 400 y 768, sino false
-
-if (mobileWindow) {
-  // La pantalla es como mínimo 400px //
+if (window.matchMedia("(min-width: 768px)").matches) {
+  // La pantalla es como mínimo 768px //
   var carousel = new bootstrap.Carousel(multipleCardCarousel, {
     interval: false,
     wrap: false,
@@ -24,16 +14,15 @@ if (mobileWindow) {
   $("#carousel .carousel-control-prev").hide(); // Escondo la flecha de retroceso de movida, ya que arranco en la primera slide //
 
   $("#carousel .carousel-control-next").on("click", function () {
-    if (scrollPosition < carouselWidth - cardWidth * (smallWindow ? 3 : 5)) {
+    if (scrollPosition < carouselWidth - cardWidth * 5) {
       scrollPosition += cardWidth; // I can multiply cardWidth for an integer, that integer indicates how many cards move when clicking the FW control //
       $("#carousel .carousel-inner").animate(
         { scrollLeft: scrollPosition },
         600
       );
       if (scrollPosition > 0) $("#carousel .carousel-control-prev").show(); // Vuelvo a mostrar la flecha de retroceso si la posición NO es la 0 //
-      if (scrollPosition >= carouselWidth - cardWidth * (smallWindow ? 3 : 5)) {
+      if (scrollPosition >= carouselWidth - cardWidth * 5) {
         $("#carousel .carousel-control-next").hide(); // Desaparece la flecha para adelantar si llegué al final del carrusel //
-        console.log("Llegué al final");
       }
     }
   });
@@ -47,7 +36,7 @@ if (mobileWindow) {
       );
       if (scrollPosition <= 0) $("#carousel .carousel-control-prev").hide(); // Escondo la flecha de retroceso //
     }
-    if (scrollPosition < carouselWidth - cardWidth * (smallWindow ? 3 : 5))
+    if (scrollPosition < carouselWidth - cardWidth * 5)
       $("#carousel .carousel-control-next").show(); // Reaparece la flecha para adelantar si se puede ir hacia adelante //
   });
 } else {
